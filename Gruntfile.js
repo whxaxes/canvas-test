bd5efdfcc7e103c391932636fc0a4404fcec3d2a
王鸿兴 <851094531@qq.com>
2015-02-26 14:08:45 +0800

    小修改

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,18 @@
 module.exports = function (grunt) {
-	grunt.loadNpmTasks('grunt-include-html');
     grunt.loadNpmTasks('grunt-html-index');
 
 	grunt.initConfig({
     	pkg : grunt.file.readJSON("package.json"),
 
-		// 使用grunt-includes构建html页面
-        includereplace: {
-            options:{
-                globals:{
-                    var1:"var1",
-                    var2:"var2"
-                }
-            },
-
-            html: {
-                cwd: 'test/html-init/',
-                expand: true,
-                src: ['**/*'],
-                dest: 'test/html/'
-            }
-        },
-
         htmlindex:{
             dist:{
-                cwd: 'test/',
                 expand: true,
-                src: ['html/**/*'],
+                src: ['src/**/*.html'],
                 dest: './menu.html'
             }
         }
     });
 
-	grunt.registerTask('run', ['includereplace:html']);
-    grunt.registerTask('go', ['htmlindex']);
+    grunt.registerTask('dev', ['htmlindex']);
 
 };
