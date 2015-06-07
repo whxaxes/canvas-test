c00248494c59776bb06add6dd187c434955dd00f
王鸿兴 <851094531@qq.com>
2015-06-07 18:37:58 +0800

    update

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var http = require('http');
 var router = require('easy-router');
 
+var port = 3030;
+
 router.init({useCache : false})
 router.setMap({
-	'**/*' : 'src/**/*'
+	'**/*' : '**/*'
 })
 
 http.createServer(function(req , res){
 	router.route(req , res);
-}).listen(3030);
+}).listen(port);
+
+console.log("服务启动...");
+console.log("http://127.0.0.1:"+port+"/menu.html");
