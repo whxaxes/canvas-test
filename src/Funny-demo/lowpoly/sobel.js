55b244fde8d0e80e783287020b6d97799a87a7aa
王鸿兴 <851094531@qq.com>
2015-06-30 14:47:18 +0800

    optimize sobel arithmetic and fix firebox's bug

--- src/Funny-demo/lowpoly/sobel.js
+++ src/Funny-demo/lowpoly/sobel.js
@@ -1,107 +1,77 @@
+//Sobel边缘检测算法
 (function(root) {
   'use strict';
 
-  function Sobel(imageData , callback) {
-    if (!(this instanceof Sobel)) {
-      return new Sobel(imageData , callback);
-    }
+  var kernelX = [
+    [-1,0,1],
+    [-2,0,2],
+    [-1,0,1]
+  ];
 
-    callback = callback || function(){}
+  var kernelY = [
+    [-1,-2,-1],
+    [0,0,0],
+    [1,2,1]
+  ];
 
-    var w = imageData.width;
-    var h = imageData.height;
-
-    var kernelX = [
-      [-1,0,1],
-      [-2,0,2],
-      [-1,0,1]
-    ];
-
-    var kernelY = [
-      [-1,-2,-1],
-      [0,0,0],
-      [1,2,1]
-    ];
-
+  function Sobel(imgdata , callback){
+    var w = imgdata.width;
+    var h = imgdata.height;
+    var data = imgdata.data;
     var sobelData = [];
-    var grayscaleData = [];
 
-    function bindPixelAt(data) {
-      return function(x, y, i) {
-        i = i || 0;
-        return data[((w * y) + x) * 4 + i];
-      };
+    //获取x、y所处像素点的rgb值，并返回平均值
+    function getAvg(x , y){
+      var i = ((w * y) + x) * 4;
+      var r = data[i];
+      var g = data[i+1];
+      var b = data[i+2];
+
+      return (r + g + b) / 3;
     }
 
-    var data = imageData.data;
-    var pixelAt = bindPixelAt(data);
-    var x, y;
+    for(var y=0;y<h;y++){
+      for(var x = 0;x<w;x++){
 
-    for (y = 0; y < h; y++) {
-      for (x = 0; x < w; x++) {
-        var r = pixelAt(x, y, 0);
-        var g = pixelAt(x, y, 1);
-        var b = pixelAt(x, y, 2);
-
-        var avg = (r + g + b) / 3;
-        grayscaleData.push(avg, avg, avg, 255);
-      }
-    }
-
-    pixelAt = bindPixelAt(grayscaleData);
-
-    for (y = 0; y < h; y++) {
-      for (x = 0; x < w; x++) {
         var pixelX = (
-            (kernelX[0][0] * pixelAt(x - 1, y - 1)) +
-            (kernelX[0][1] * pixelAt(x, y - 1)) +
-            (kernelX[0][2] * pixelAt(x + 1, y - 1)) +
-            (kernelX[1][0] * pixelAt(x - 1, y)) +
-            (kernelX[1][1] * pixelAt(x, y)) +
-            (kernelX[1][2] * pixelAt(x + 1, y)) +
-            (kernelX[2][0] * pixelAt(x - 1, y + 1)) +
-            (kernelX[2][1] * pixelAt(x, y + 1)) +
-            (kernelX[2][2] * pixelAt(x + 1, y + 1))
+          (kernelX[0][0] * getAvg(x - 1, y - 1)) +
+          (kernelX[0][1] * getAvg(x, y - 1)) +
+          (kernelX[0][2] * getAvg(x + 1, y - 1)) +
+          (kernelX[1][0] * getAvg(x - 1, y)) +
+          (kernelX[1][1] * getAvg(x, y)) +
+          (kernelX[1][2] * getAvg(x + 1, y)) +
+          (kernelX[2][0] * getAvg(x - 1, y + 1)) +
+          (kernelX[2][1] * getAvg(x, y + 1)) +
+          (kernelX[2][2] * getAvg(x + 1, y + 1))
         );
 
         var pixelY = (
-          (kernelY[0][0] * pixelAt(x - 1, y - 1)) +
-          (kernelY[0][1] * pixelAt(x, y - 1)) +
-          (kernelY[0][2] * pixelAt(x + 1, y - 1)) +
-          (kernelY[1][0] * pixelAt(x - 1, y)) +
-          (kernelY[1][1] * pixelAt(x, y)) +
-          (kernelY[1][2] * pixelAt(x + 1, y)) +
-          (kernelY[2][0] * pixelAt(x - 1, y + 1)) +
-          (kernelY[2][1] * pixelAt(x, y + 1)) +
-          (kernelY[2][2] * pixelAt(x + 1, y + 1))
+          (kernelY[0][0] * getAvg(x - 1, y - 1)) +
+          (kernelY[0][1] * getAvg(x, y - 1)) +
+          (kernelY[0][2] * getAvg(x + 1, y - 1)) +
+          (kernelY[1][0] * getAvg(x - 1, y)) +
+          (kernelY[1][1] * getAvg(x, y)) +
+          (kernelY[1][2] * getAvg(x + 1, y)) +
+          (kernelY[2][0] * getAvg(x - 1, y + 1)) +
+          (kernelY[2][1] * getAvg(x, y + 1)) +
+          (kernelY[2][2] * getAvg(x + 1, y + 1))
         );
 
         var magnitude = Math.sqrt((pixelX * pixelX) + (pixelY * pixelY))>>0;
 
         sobelData.push(magnitude, magnitude, magnitude, 255);
 
-        callback(magnitude , x , y)
+        callback(magnitude , x , y);
       }
     }
 
     try{
       return new ImageData(new Uint8ClampedArray(sobelData), w, h);
     }catch(e){
-      return {data : sobelData};
+      return new Uint8ClampedArray(sobelData);
     }
   }
 
-  if (typeof exports !== 'undefined') {
-    if (typeof module !== 'undefined' && module.exports) {
-      exports = module.exports = Sobel;
-    }
-    exports.Sobel = Sobel;
-  } else if (typeof define === 'function' && define.amd) {
-    define([], function() {
-      return Sobel;
-    });
-  } else {
-    root.Sobel = Sobel;
-  }
+  root.Sobel = Sobel;
 
 })(this);
