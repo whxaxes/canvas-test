767e84a6e069016ffccb5cfbcfdf56bd398b951b
王鸿兴 <851094531@qq.com>
2015-07-12 11:57:42 +0800

    update

--- src/Funny-demo/transform/test.js
+++ src/Funny-demo/transform/test.js
@@ -1,13 +1,44 @@
 !function(){
+    //是否显示点的checkbox
+    var dotChoose = document.getElementById("dot");
+    //是否显示方格的checkbox
+    var rectChoose = document.getElementById("rect");
+    //是否显示贴图的checkbox
+    var picChoose = document.getElementById("pic");
+    //将图片分割的分数控制
+    var countChoose = document.getElementById("count");
+
+    var hasDot = dotChoose.checked;
+    var hasRect = rectChoose.checked;
+    var hasPic = picChoose.checked;
+    var count = getSelected();
+
+    dotChoose.onchange = function(){ hasDot = this.checked;render(); };
+    rectChoose.onchange = function(){ hasRect = this.checked;render(); };
+    picChoose.onchange = function(){ hasPic = this.checked;render(); };
+    countChoose.onchange = function(){
+        count = getSelected();
+        //count更改后需要重新计算所有点的初始位置
+        idots = rectsplit(count, dotscopy[0], dotscopy[1], dotscopy[2], dotscopy[3]);
+        render();
+    };
+
+    function getSelected(){
+        var ops = countChoose.getElementsByTagName("OPTION") ,op;
+        for(var i=0;i<ops.length;i++){
+            op = ops[i];
+            if(op.selected)return +op.value;
+        }
+    }
+
     var canvas = document.getElementById("cas");
     var ctx = canvas.getContext("2d");
 
-    var dr = 4;
     var dots = [];
-    var idots;
+    var dotscopy , idots;
 
     var img = new Image();
-    img.src = "./test2.jpg";
+    img.src = Math.random()>0.5?"./img/test2.jpg":"./img/test.jpg";
     img.onload = function(){
         var img_w = img.width/2;
         var img_h = img.height/2;
@@ -24,12 +55,16 @@
             { x:left, y:top + img_h}
         ];
 
-        idots = [
+        //保存一份不变的拷贝
+        dotscopy = [
             { x:left, y:top },
             { x:left + img_w, y:top },
             { x:left + img_w, y:top + img_h},
             { x:left, y:top + img_h}
         ];
+
+        //获得所有初始点坐标
+        idots = rectsplit(count, dotscopy[0], dotscopy[1], dotscopy[2], dotscopy[3]);
 
         render()
     };
@@ -39,10 +74,12 @@
 
         var area = getArea(e);
         var dot,i;
+        //鼠标事件触发区域
+        var qy = 40;
 
         for (i = 0; i < dots.length; i++) {
             dot = dots[i];
-            if (area.t >= (dot.y - dr) && area.t <= (dot.y + dr) && area.l >= (dot.x - dr) && area.l <= (dot.x + dr)) {
+            if (area.t >= (dot.y - qy) && area.t <= (dot.y + qy) && area.l >= (dot.x - qy) && area.l <= (dot.x + qy)) {
                 break;
             } else {
                 dot = null;
@@ -78,74 +115,54 @@
         }
     }
 
+    /**
+     * 画布渲染方法
+     */
     function render(){
         ctx.clearRect(0,0,canvas.width,canvas.height);
 
-        ctx.fillStyle = "#fff";
-        dots.forEach(function(d , i){
-            ctx.fillRect(d.x - dr, d.y - dr, dr * 2, dr * 2);
-            ctx.strokeRect(d.x - dr, d.y - dr, dr * 2, dr * 2);
-        });
-        ctx.restore();
+        var ndots = rectsplit(count, dots[0], dots[1], dots[2], dots[3]);
 
-        var ndots = caculate(100);
-
-        ctx.save();
-        ctx.fillStyle = "red";
         ndots.forEach(function(d , i){
-            ctx.fillRect(d.x-1 , d.y-1 , 2 , 2)
+            //获取平行四边形的四个点
+            var dot1 = ndots[i];
+            var dot2 = ndots[i + 1];
+            var dot3 = ndots[i + count + 2];
+            var dot4 = ndots[i + count + 1];
+
+            //获取初始平行四边形的四个点
+            var idot1 = idots[i];
+            var idot2 = idots[i + 1];
+            var idot3 = idots[i + count + 2];
+            var idot4 = idots[i + count + 1];
+
+            if (dot2 && dot3 && i%(count+1)<count){
+                //绘制三角形的下半部分
+                renderImage(idot3, dot3, idot2, dot2, idot4, dot4);
+
+                //绘制三角形的上半部分
+                renderImage(idot1, dot1, idot2, dot2, idot4, dot4);
+            }
+
+            if(hasDot){
+                ctx.save();
+                ctx.fillStyle = "red";
+                ctx.fillRect(d.x-1 , d.y-1 , 2 , 2);
+                ctx.save();
+            }
         });
-        ctx.restore();
-
-//        renderByAlge(idots[2] , dots[2], idots[1] , dots[1] , idots[3] , dots[3]);
-//        renderByAlge(idots[0] , dots[0], idots[1] , dots[1] , idots[3] , dots[3]);
-    }
-
-
-    //将方框n等分，获取所有点坐标
-    function caculate(n){
-        //ad向量方向n等分
-        var ad_x = (dots[3].x - dots[0].x)/n;
-        var ad_y = (dots[3].y - dots[0].y)/n;
-        //bc向量方向n等分
-        var bc_x = (dots[2].x - dots[1].x)/n;
-        var bc_y = (dots[2].y - dots[1].y)/n;
-
-        var ndots = [];
-        var dot , dot2;
-        var ab_x,ab_y;
-        //左边点递增，右边点递增，获取每一次递增后的新的向量，继续n等分，从而获取所有点坐标
-        for(var i=0;i<=n;i++){
-            dot = {
-                x: dots[0].x + ad_x * i,
-                y: dots[0].y + ad_y * i
-            };
-            dot2 = {
-                x: dots[1].x + bc_x * i,
-                y: dots[1].y + bc_y * i
-            }
-            for(var j=0;j<=n;j++){
-                //ab向量方向n等分
-                ab_x = (dot2.x - dot.x)/n;
-                ab_y = (dot2.y - dot.y)/n;
-
-                ndots.push({
-                    x: dot.x + ab_x * j,
-                    y: dot.y + ab_y * j
-                })
-            }
-        }
-
-        var rects = [];
-
-
-        return ndots;
     }
 
     /**
-     * 使用代数法计算矩阵
+     * 计算矩阵，同时渲染图片
+     * @param arg_1
+     * @param _arg_1
+     * @param arg_2
+     * @param _arg_2
+     * @param arg_3
+     * @param _arg_3
      */
-    function renderByAlge(arg_1 , _arg_1 , arg_2 , _arg_2 , arg_3 , _arg_3){
+    function renderImage(arg_1 , _arg_1 , arg_2 , _arg_2 , arg_3 , _arg_3){
         ctx.save();
         //根据变换后的坐标创建剪切区域
         ctx.beginPath();
@@ -153,70 +170,69 @@
         ctx.lineTo(_arg_2.x, _arg_2.y);
         ctx.lineTo(_arg_3.x, _arg_3.y);
         ctx.closePath();
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = "red";
-        ctx.stroke();
+        if(hasRect){
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = "red";
+            ctx.stroke();
+        }
         ctx.clip();
 
-        //传入变换前后的点坐标，计算变换矩阵
-        var matrix = getMatrix.apply(this , arguments);
+        if(hasPic){
+            //传入变换前后的点坐标，计算变换矩阵
+            var matrix = getMatrix.apply(this , arguments);
 
-        //变形
-        ctx.transform(matrix.a , matrix.b , matrix.c , matrix.d , matrix.e , matrix.f);
+            //变形
+            ctx.transform(matrix.a , matrix.b , matrix.c , matrix.d , matrix.e , matrix.f);
 
-        //绘制图片
-        ctx.drawImage(img , idots[0].x , idots[0].y , img.width , img.height);
+            //绘制图片
+            ctx.drawImage(img , idots[0].x , idots[0].y , img.width , img.height);
+        }
 
         ctx.restore();
     }
 
+
     /**
-     * 根据向量来计算矩阵
-     * @param dot1
-     * @param dot2
-     * @param dot3
-     * @param isf
+     * 将abcd四边形分割成n的n次方份，获取n等分后的所有点坐标
+     * @param n     多少等分
+     * @param a     a点坐标
+     * @param b     b点坐标
+     * @param c     c点坐标
+     * @param d     d点坐标
+     * @returns {Array}
      */
-    function renderByVector(dot1 , dot2 , dot3 , isf){
-        ctx.save();
-        ctx.beginPath();
-        ctx.moveTo(dot1.x, dot1.y);
-        ctx.lineTo(dot2.x, dot2.y);
-        ctx.lineTo(dot3.x, dot3.y);
-        ctx.closePath();
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = "red";
-        ctx.stroke();
-        ctx.clip();
+    function rectsplit(n , a , b , c , d){
+        //ad向量方向n等分
+        var ad_x = (d.x - a.x)/n;
+        var ad_y = (d.y - a.y)/n;
+        //bc向量方向n等分
+        var bc_x = (c.x - b.x)/n;
+        var bc_y = (c.y - b.y)/n;
 
-        //点1指向点2的 向量1
-        var xl1 = [dot2.x-dot1.x , dot2.y - dot1.y];
+        var ndots = [];
+        var x1, y1, x2, y2, ab_x, ab_y;
 
-        //点1指向点3的 向量2
-        var xl2 = [dot3.x-dot1.x , dot3.y - dot1.y];
+        //左边点递增，右边点递增，获取每一次递增后的新的向量，继续n等分，从而获取所有点坐标
+        for(var i=0;i<=n;i++){
+            //获得ad向量n等分后的坐标
+            x1 = a.x + ad_x * i;
+            y1 = a.y + ad_y * i;
+            //获得bc向量n等分后的坐标
+            x2 = b.x + bc_x * i;
+            y2 = b.y + bc_y * i;
 
-        //向量1和向量2的向量和
-        var xl3 = [xl1[0] + xl2[0] , xl1[1] + xl2[1]];
+            for(var j=0;j<=n;j++){
+                //ab向量为：[x2 - x1 , y2 - y1]，所以n等分后的增量为除于n
+                ab_x = (x2 - x1)/n;
+                ab_y = (y2 - y1)/n;
 
-        //算出平行四边形的第四个端点坐标
-        var edot = { x:xl3[0] + dot1.x, y:xl3[1] + dot1.y };
+                ndots.push({
+                    x: x1 + ab_x * j,
+                    y: y1 + ab_y * j
+                })
+            }
+        }
 
-        //平行四边形中点坐标
-        var mdot = { x:(edot.x + dot1.x)/2, y:(edot.y + dot1.y)/2 };
-
-        //基本点坐标，是以哪个点为基准绘制图片
-        var base = isf ? edot : dot1;
-
-        //基于x轴的斜切 和 基于y轴的斜切
-        var xq = (dot2.y - base.y) / (dot2.x - base.x);
-        var yq = (dot3.x - base.x) / (dot3.y - base.y);
-
-        var img_w = Math.abs(dot2.x - base.x);
-        var img_h = Math.abs(dot3.y - base.y);
-
-        ctx.setTransform(1, xq, yq, 1, mdot.x , mdot.y);
-        ctx.drawImage(img , -img_w/2 , -img_h/2 , img_w , img_h);
-
-        ctx.restore();
+        return ndots;
     }
 }();
