3372c4b91e366ab9fb239a643481963d480430d9
wanghx <whxaxes@gmail.com>
2018-03-13 10:09:24 +0800

    refactor: format code

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var Router = require('easy-router');
 
 var port = 3030;
 
-Router().setMap('**/*', '**/*').listen(port);
+Router()
+  .setMap('**/*', '**/*')
+  .listen(port);
 
-console.log("http://127.0.0.1:"+port+"/menu.html");
+console.log('http://127.0.0.1:' + port + '/menu.html');
